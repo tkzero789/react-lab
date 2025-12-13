fe2f2c50de68b1641588f65d12264e118cdc8d69
Thinh Tran <thinhtran0061@gmail.com>
2025-12-12 23:49:51 -0500

    refactor: adjust the root route UI

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -1,18 +1,59 @@
 import type { Metadata } from "next";
 import DashboardBreadcrumb from "./components/dashboard-breadcrumb";
+import DashboardContainer from "@/components/layout/dashboard-container";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Apps",
 };
 
+const apps = [
+  {
+    title: "Todo",
+    href: "/apps/todo",
+  },
+  {
+    title: "Weather",
+    href: "/apps/weather",
+  },
+  {
+    title: "Movie",
+    href: "/apps/movie",
+  },
+];
+
 export default function AppsPage() {
   return (
-    <DashboardBreadcrumb
-      breadcrumbs={[
-        {
-          title: "Apps",
-        },
-      ]}
-    />
+    <>
+      <DashboardBreadcrumb
+        breadcrumbs={[
+          {
+            title: "Apps",
+          },
+        ]}
+      />
+      <DashboardContainer className="grid grid-cols-1 gap-4 lg:grid-cols-4">
+        {apps.map((app) => (
+          <Link key={app.title} href={app.href}>
+            <Card>
+              <CardHeader className="pb-4">
+                <CardTitle>{app.title}</CardTitle>
+              </CardHeader>
+              {/* <CardContent> */}
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              {/* <img
+                  src="https://miro.medium.com/max/2000/1*TEjMGT3zZKz2MaAZBZkXhw.png"
+                  alt="app image"
+                  width={200}
+                  height={100}
+                  className="h-36 w-full rounded-xl"
+                /> */}
+              {/* </CardContent> */}
+            </Card>
+          </Link>
+        ))}
+      </DashboardContainer>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,42 +9,42 @@ const cardItems = [
     iconBackground: "bg-gradient-to-b from-yellow-300/70  to-yellow-600/80",
     title: "Three.js",
     href: "/threejs",
-    hoverBackground: "hover:bg-yellow-300/30",
+    hoverBackground: "hover:bg-yellow-400/40",
   },
   {
     icon: Replicas,
     iconBackground: "bg-gradient-to-b from-green-300/70 to-green-500",
     title: "Replicas",
     href: "/replicas",
-    hoverBackground: "hover:bg-green-300/30",
+    hoverBackground: "hover:bg-green-400/40",
   },
   {
     icon: Nextjs,
     iconBackground: "bg-gradient-to-b from-slate-400/50 to-slate-700/90",
     title: "Apps",
     href: "/apps",
-    hoverBackground: "hover:bg-slate-300/30",
+    hoverBackground: "hover:bg-slate-400/40",
   },
 ];
 
 export default function Page() {
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-center gap-16">
-      <h1 className="text-4xl font-bold">React Lab</h1>
+      <h1>React Lab</h1>
       <div className="grid place-items-center gap-8 md:grid-cols-3">
         {cardItems.map((item) => (
           <Link
             href={item.href}
             key={item.title}
-            className={`group size-40 rounded-xl bg-neutral-100 p-2 transition-all duration-300 lg:size-56 ${item.hoverBackground}`}
+            className={`group size-40 rounded-xl bg-muted p-2 transition-all duration-300 lg:size-48 ${item.hoverBackground}`}
           >
-            <div className="flex h-full flex-col items-center justify-center gap-2 rounded-xl border bg-white shadow-sm">
+            <div className="flex h-full flex-col items-center justify-center gap-2 rounded-xl bg-background">
               <div
-                className={`flex size-14 items-center justify-center rounded-xl p-4 transition-all duration-300 group-hover:scale-105 ${item.iconBackground}`}
+                className={`flex size-14 items-center justify-center rounded-xl p-2 transition-all duration-300 group-hover:scale-105 ${item.iconBackground}`}
               >
                 <item.icon />
               </div>
-              <div className="flex items-center justify-center text-lg font-semibold">
+              <div className="flex items-center justify-center font-semibold">
                 {item.title}
               </div>
             </div>

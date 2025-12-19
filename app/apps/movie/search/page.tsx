a700cb1b0238df759bcbfef8e86beb23b9352470
Thinh Tran <thinhtran0061@gmail.com>
2025-12-18 23:23:51 -0500

    refactor: clean up unused code

--- app/apps/movie/search/page.tsx
+++ app/apps/movie/search/page.tsx
@@ -1,47 +1,44 @@
 import React from "react";
 import MovieSearch from "../components/movie-search";
-import MovieCard from "../components/movie-card";
-import SearchPagination from "./components/SearchPagination";
+// import SearchPagination from "./components/SearchPagination";
 
-type Movie = {
-  _id: string;
-  name: string;
-  slug: string;
-  poster_url: string;
-  year: number;
-};
+// type Movie = {
+//   _id: string;
+//   name: string;
+//   slug: string;
+//   poster_url: string;
+//   year: number;
+// };
 
-type Props = {
-  searchParams: {
-    s: string;
-    page: number;
-  };
-};
+// type Props = {
+//   searchParams: {
+//     s: string;
+//     page: number;
+//   };
+// };
 
-export default async function MovieSearchPage({ searchParams }: Props) {
-  const searchTerm = searchParams?.s;
-  const currentPage = searchParams?.page;
+export default async function MovieSearchPage() {
+  // const searchTerm = searchParams?.s;
+  // const currentPage = searchParams?.page;
 
-  const formatSearchTerm = searchTerm.trim().split(" ").join("-");
-  const res = await fetch(
-    `https://phimapi.com/v1/api/tim-kiem?keyword=${formatSearchTerm}`,
-  );
-  const data = await res.json();
-  const totalPage = Math.ceil(data.data.items?.length / 10);
+  // const formatSearchTerm = searchTerm.trim().split(" ").join("-");
+  // const res = await fetch(
+  //   `https://phimapi.com/v1/api/tim-kiem?keyword=${formatSearchTerm}`,
+  // );
+  // const data = await res.json();
+  // const totalPage = Math.ceil(data.data.items?.length / 10);
 
-  let filteredMovies: Movie[] = [];
+  // let filteredMovies: Movie[] = [];
 
-  const startIndex = (Number(currentPage) - 1) * 10;
-  const endIndex = Number(currentPage) * 10;
+  // const startIndex = (Number(currentPage) - 1) * 10;
+  // const endIndex = Number(currentPage) * 10;
 
-  filteredMovies = data.data.items
-    ?.slice(startIndex, endIndex)
-    .map((item: Movie) => ({
-      ...item,
-      poster_url: `https://phimimg.com/${item.poster_url}`,
-    }));
-
-  console.log(data.data.items);
+  // filteredMovies = data.data.items
+  //   ?.slice(startIndex, endIndex)
+  //   .map((item: Movie) => ({
+  //     ...item,
+  //     poster_url: `https://phimimg.com/${item.poster_url}`,
+  //   }));
 
   return (
     <div>
@@ -50,12 +47,12 @@ export default async function MovieSearchPage({ searchParams }: Props) {
       <div className="mt-8 flex items-center justify-between">
         <MovieSearch />
       </div>
-      <div className="mt-8 grid grid-cols-5 gap-x-4 gap-y-8">
+      {/* <div className="mt-8 grid grid-cols-5 gap-x-4 gap-y-8">
         {filteredMovies?.map((movie: Movie) => (
           <MovieCard key={movie._id} movie={movie} />
         ))}
-      </div>
-      <SearchPagination totalPage={totalPage} />
+      </div> */}
+      {/* <SearchPagination totalPage={totalPage} /> */}
     </div>
   );
 }

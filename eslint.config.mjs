83977ae27e5b9efcda4a42d8b4a5d52a251b8e0f
Thinh Tran <thinhtran0061@gmail.com>
2026-04-09 21:47:00 -0400

    feat: add TypeScript ESLint plugin to configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 import nextConfig from "eslint-config-next";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   ...nextConfig,
   {
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "warn",

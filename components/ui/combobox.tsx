d4dd3b2af25b0194140df48e2838dea6fbdfbea2
Thinh Tran <thinhtran0061@gmail.com>
2026-05-25 14:07:31 -0400

    feat: cleanup combobox component

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -33,7 +33,7 @@ function ComboboxTrigger({
       {children}
       <ChevronDownIcon
         data-slot="combobox-trigger-icon"
-        className="pointer-events-none size-4 text-gray-500 dark:text-gray-400"
+        className="pointer-events-none size-4 "
       />
     </ComboboxPrimitive.Trigger>
   );
@@ -116,7 +116,7 @@ function ComboboxContent({
           data-slot="combobox-content"
           data-chips={!!anchor}
           className={cn(
-            "group/combobox-content w-(--anchor-width) max-w-(--available-width) origin-(--transform-origin) data-[chips=true]:min-w-(--anchor-width) data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 relative max-h-96 min-w-[calc(var(--anchor-width)+1.75rem)] overflow-hidden rounded-xl bg-white text-gray-950 shadow-md ring-1 ring-gray-950/10 duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[slot=input-group]:*:m-1 data-[slot=input-group]:*:mb-0 data-[slot=input-group]:*:h-8 data-[slot=input-group]:*:border-gray-200/30 data-[slot=input-group]:*:bg-gray-200/30 data-[slot=input-group]:*:shadow-none dark:bg-gray-950 dark:text-gray-50 dark:ring-gray-50/10 dark:data-[slot=input-group]:*:border-gray-800/30 dark:data-[slot=input-group]:*:bg-gray-800/30",
+            "group/combobox-content w-(--anchor-width) max-w-(--available-width) origin-(--transform-origin) data-[chips=true]:min-w-(--anchor-width) data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 relative max-h-96 min-w-[calc(var(--anchor-width)+1.75rem)] overflow-hidden rounded-xl  shadow-md ring-1 ring-border bg-background duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[slot=input-group]:*:m-1 data-[slot=input-group]:*:mb-0 data-[slot=input-group]:*:h-8 data-[slot=input-group]:*:border data-[slot=input-group]:*:bg-muted data-[slot=input-group]:*:shadow-none",
             className,
           )}
           {...props}
@@ -131,7 +131,7 @@ function ComboboxList({ className, ...props }: ComboboxPrimitive.List.Props) {
     <ComboboxPrimitive.List
       data-slot="combobox-list"
       className={cn(
-        "data-empty:p-0 max-h-[min(calc(--spacing(96)-(--spacing(9))),calc(var(--available-height)-(--spacing(9))))] scroll-py-1 overflow-y-auto p-1",
+        "data-empty:p-0 scroll-py-1 overflow-y-auto p-1",
         className,
       )}
       {...props}
@@ -148,7 +148,7 @@ function ComboboxItem({
     <ComboboxPrimitive.Item
       data-slot="combobox-item"
       className={cn(
-        "outline-hidden data-highlighted:bg-gray-100 data-highlighted:text-gray-900 dark:data-highlighted:bg-gray-800 dark:data-highlighted:text-gray-50 relative flex w-full cursor-default select-none items-center gap-2 rounded-xl py-1.5 pl-2 pr-8 text-sm hover:bg-accent data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+        "outline-hidden relative flex w-full cursor-default select-none items-center gap-2 rounded-xl py-1.5 pl-2 pr-8 text-sm hover:bg-accent data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
         className,
       )}
       {...props}
@@ -184,7 +184,7 @@ function ComboboxLabel({
     <ComboboxPrimitive.GroupLabel
       data-slot="combobox-label"
       className={cn(
-        "pointer-coarse:px-3 pointer-coarse:py-2 pointer-coarse:text-sm px-2 py-1.5 text-xs text-gray-500 dark:text-gray-400",
+        "pointer-coarse:px-3 pointer-coarse:py-2 pointer-coarse:text-sm px-2 py-1.5 text-xs ",
         className,
       )}
       {...props}
@@ -203,7 +203,7 @@ function ComboboxEmpty({ className, ...props }: ComboboxPrimitive.Empty.Props) {
     <ComboboxPrimitive.Empty
       data-slot="combobox-empty"
       className={cn(
-        "group-data-empty/combobox-content:flex hidden w-full justify-center py-2 text-center text-sm text-gray-500 dark:text-gray-400",
+        "group-data-empty/combobox-content:flex hidden w-full justify-center py-2 text-center text-sm ",
         className,
       )}
       {...props}
@@ -218,7 +218,7 @@ function ComboboxSeparator({
   return (
     <ComboboxPrimitive.Separator
       data-slot="combobox-separator"
-      className={cn("-mx-1 my-1 h-px bg-gray-200 dark:bg-gray-800", className)}
+      className={cn("-mx-1 my-1 h-px ", className)}
       {...props}
     />
   );
@@ -233,7 +233,7 @@ function ComboboxChips({
     <ComboboxPrimitive.Chips
       data-slot="combobox-chips"
       className={cn(
-        "shadow-2xs has-aria-invalid:border-red-500 has-aria-invalid:ring-[3px] has-aria-invalid:ring-red-500/20 has-data-[slot=combobox-chip]:px-1.5 dark:has-aria-invalid:border-red-500/50 dark:has-aria-invalid:ring-red-500/40 dark:has-aria-invalid:border-red-900 dark:has-aria-invalid:ring-red-900/20 dark:dark:has-aria-invalid:border-red-900/50 dark:dark:has-aria-invalid:ring-red-900/40 flex min-h-9 flex-wrap items-center gap-1.5 rounded-md border border-gray-200 bg-transparent bg-clip-padding px-2.5 py-1.5 text-sm transition-[color,box-shadow] focus-within:border-gray-950 focus-within:ring-[3px] focus-within:ring-gray-950/50 dark:border-gray-800 dark:bg-gray-200/30 dark:dark:bg-gray-800/30 dark:focus-within:border-gray-300 dark:focus-within:ring-gray-300/50",
+        "shadow-2xs has-aria-invalid:border-destructive has-aria-invalid:ring-[3px] has-aria-invalid:ring-destructive has-data-[slot=combobox-chip]:px-1.5  flex min-h-9 flex-wrap items-center gap-1.5 rounded-md border bg-transparent bg-clip-padding px-2.5 py-1.5 text-sm transition-[color,box-shadow]",
         className,
       )}
       {...props}
@@ -253,7 +253,7 @@ function ComboboxChip({
     <ComboboxPrimitive.Chip
       data-slot="combobox-chip"
       className={cn(
-        "has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50 has-data-[slot=combobox-chip-remove]:pr-0 flex h-[calc(--spacing(5.5))] w-fit items-center justify-center gap-1 whitespace-nowrap rounded-sm bg-gray-100 px-1.5 text-xs font-medium text-gray-950 dark:bg-gray-800 dark:text-gray-50",
+        "has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50 has-data-[slot=combobox-chip-remove]:pr-0 flex w-fit items-center justify-center gap-1 whitespace-nowrap rounded-sm bg-background px-1.5 text-xs font-medium",
         className,
       )}
       {...props}
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -246,7 +246,7 @@ const Sidebar = React.forwardRef<
             "group-data-[collapsible=offcanvas]:w-0",
             "group-data-[side=right]:rotate-180",
             variant === "floating" || variant === "inset"
-              ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]"
+              ? ""
               : "group-data-[collapsible=icon]:w-(--sidebar-width-icon)",
           )}
         />
@@ -258,7 +258,7 @@ const Sidebar = React.forwardRef<
               : "right-0 group-data-[collapsible=offcanvas]:-right-(--sidebar-width)",
             // Adjust the padding for floating and inset variants.
             variant === "floating" || variant === "inset"
-              ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]"
+              ? "p-2"
               : "group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l",
             className,
           )}

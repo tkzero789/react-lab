2f39c09252e7ad11c90e3e8a49871767f718238f
Thinh Tran <thinhtran0061@gmail.com>
2026-07-29 18:36:44 -0400

    feat: update next.js package and replace loader

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -4,8 +4,6 @@ export default function Loader() {
       role="status"
       aria-label="Loading"
       className="loader absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-    >
-      LAB
-    </div>
+    />
   )
 }
